73736d12364acc3a7a125e6e74b0d2d2c65fed8f
Valentyn Fomenko <fomenko.vn@gmail.com>
2019-12-22 19:00:26 +0200

    build

---
+++ public/precache-manifest.0c5e5073aebc167cd5fea8254242e2f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a964208dc7f8a7ae21a628c6ef5fd5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d63285ef93b18a13fa09",
+    "url": "/static/css/main.97b5d469.chunk.css"
+  },
+  {
+    "revision": "bce623944f628fce176c",
+    "url": "/static/js/2.fa377a12.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/static/js/2.fa377a12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d63285ef93b18a13fa09",
+    "url": "/static/js/main.e658da72.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
