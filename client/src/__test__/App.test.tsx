71521e0248b40d8d0cd6b5472693198dc3d5ac2c
Valentyn Fomenko <fomenko.vn@gmail.com>
2020-01-05 17:21:08 +0200

    test: add snapshot test, mv tests to __test__ folder

--- client/src/__test__/App.test.tsx
+++ client/src/__test__/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders header', () => {
   const { getByText } = render(<App />);
   const headerElement = getByText(/spacex/i);
   expect(headerElement).toBeInTheDocument();
+});
+
+test('matches snapshot', () => {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
 });
