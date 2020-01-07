c7bc3d0a3efa9757b938ea171445cdaa00a9e038
Valentyn Fomenko <fomenko.vn@gmail.com>
2020-01-07 20:51:41 +0200

    refactor: move mock to separate file

--- client/src/__test__/Launch.test.tsx
+++ client/src/__test__/Launch.test.tsx
@@ -2,34 +2,8 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/react-testing';
 import { render, wait } from '@testing-library/react';
-import { Launch, GET_LAUNCH } from 'components/Launch';
-
-const mocks = [
-  {
-    request: {
-      query: GET_LAUNCH,
-      variables: {
-        flight_number: 1,
-      },
-    },
-    result: {
-      data: {
-        launch: {
-          flight_number: 1,
-          mission_name: 'FalconSat',
-          launch_year: '2006',
-          launch_date_local: '2006-03-25T10:30:00+12:00',
-          launch_success: false,
-          rocket: {
-            rocket_id: 'falcon1',
-            rocket_name: 'Falcon 1',
-            rocket_type: 'Merlin A',
-          },
-        },
-      },
-    },
-  },
-];
+import { Launch } from 'components/Launch';
+import { mocks } from './mocks';
 
 it('Launch should render loading state initially', () => {
   const { getByTestId } = render(
--- client/src/__test__/Launches.test.tsx
+++ client/src/__test__/Launches.test.tsx
@@ -2,46 +2,9 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/react-testing';
 import { render, wait } from '@testing-library/react';
-import { Launches, GET_LAUNCHES } from 'components/Launches';
+import { Launches } from 'components/Launches';
 import { renderWithRouter } from './testUtils';
-
-const mocks = [
-  {
-    request: {
-      query: GET_LAUNCHES,
-    },
-    result: {
-      data: {
-        launches: [
-          {
-            flight_number: 1,
-            mission_name: 'FalconSat',
-            launch_year: '2006',
-            launch_date_local: '2006-03-25T10:30:00+12:00',
-            launch_success: false,
-            rocket: {
-              rocket_id: 'falcon1',
-              rocket_name: 'Falcon 1',
-              rocket_type: 'Merlin A',
-            },
-          },
-          {
-            flight_number: 2,
-            mission_name: 'DemoSat',
-            launch_year: '2007',
-            launch_date_local: '2007-03-21T13:10:00+12:00',
-            launch_success: false,
-            rocket: {
-              rocket_id: 'falcon1',
-              rocket_name: 'Falcon 1',
-              rocket_type: 'Merlin A',
-            },
-          },
-        ],
-      },
-    },
-  },
-];
+import { mocks } from './mocks';
 
 it('Launches should render loading state initially', () => {
   const { getByTestId } = render(
