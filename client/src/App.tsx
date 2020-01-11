ac0aaf40e053b25d00a46beca593e7d8f6bac1ed
Valentyn Fomenko <fomenko.vn@gmail.com>
2020-01-11 12:05:57 +0200

    add theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CoreLayout } from 'layouts/CoreLayout';
+import 'semantic-ui-css/semantic.min.css';
 
 const client = new ApolloClient({
   uri: '/graphql',
