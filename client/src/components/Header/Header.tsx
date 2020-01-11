cd3094cb13bbff487013b90ca589fdd9fc66dbab
Valentyn Fomenko <fomenko.vn@gmail.com>
2020-01-11 12:06:05 +0200

    add header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { Header as SemanticHeader } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-import styles from './Header.module.css';
 
 export const Header: React.FC<{}> = () => (
-  <header className={styles.header} data-testid="header">
+  <SemanticHeader data-testid="header">
     <Link to="/">SpaceX</Link>
-  </header>
+  </SemanticHeader>
 );
